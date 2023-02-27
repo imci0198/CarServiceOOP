96e1390a5b6a6c5c2f51a2b81898c63742e42ae5
imci0198 <imci0198@scs.ubbcluj.ro>
2023-02-27 15:19:50 +0200

    Console + UserInterface

--- src/UserInterface/Console.java
+++ src/UserInterface/Console.java
@@ -2,10 +2,47 @@ package UserInterface;
 
 import Service.ClientEntryService;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Console {
     public ClientEntryService clientEntryService;
 
     public Console (ClientEntryService clientEntryService){
         this.clientEntryService = clientEntryService;
     }
-}
+
+
+    public void runUserInterface(){
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            showMenu();
+            String option = in.nextLine();
+            if("1".equals(option)){
+                this.handleAddClientEntry();
+            }
+        }
+    }
+
+    private void handleAddClientEntry() {
+        try {
+            Scanner in = new Scanner(System.in);
+            System.out.println("Give entry ID:");
+            int idClientEntry = in.nextInt();
+            System.out.println("Give stand number:");
+            int standNumber = in.nextInt();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+                private void showMenu() {
+                System.out.println("1.Service entry");
+                System.out.println("2.Left service");
+                System.out.println("3.Show all stands");
+                System.out.println("4.Show all entries");
+                System.out.println("x.Exit");
+                System.out.print("Optiune");
+            }
+            }
